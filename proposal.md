d3fdfde46a194ca16d068587a69276e398d034d2
Thuyan Nguyen <tpn210003@utdallas.edu>
2025-11-25 17:08:44 -0600

    Basic function V1

--- proposal.md
+++ proposal.md
@@ -6,6 +6,7 @@ Take files from any software on the PC and ensure proper naming convention that 
  2. Ask what folder it needs to be sorted into
  3. Read the names of other files within that folder, and match the naming convention numerically
  4. Update file placement when the name of a file is updated
+ 5. Filtering system by file name and type
 
 ### challenges
  1. matching names between files and folders
---
+++ src.py
@@ -0,0 +1,28 @@
+"""Take files from any software on the PC and
+ensure proper naming convention that can be set,
+then place it in the proper folder.
+
+ 1. Read the file name and type
+ 2. Ask what folder it needs to be sorted into
+ 3. Read the names of other files within that folder, and match the naming convention numerically
+ 4. Update file placement when the name of a file is updated
+ 5. Filtering system by file name and type
+
+  ideally, this project will be able to interface with OBS,
+  Obsidian, browser  Downloads, Maya, Adobe, and Clip Studio Paint
+"""
+import shutil
+import os
+
+unsorted_folder = "C:\Users"
+project_folder_path = "C:\Users"
+
+for filename in os.listdir(unsorted_folder):
+    path, filetype = os.path.splitext(filename)
+    filename_category = filename.split("_")
+    folder_location = filename_category[0]
+    try:
+        os.mkdir(f"{filetype}")
+    except:
+        print(f"Folder for {filetype} exists")
+    shutil.move(unsorted_folder, f"{project_folder_path}/{folder_location}/{filetype}")

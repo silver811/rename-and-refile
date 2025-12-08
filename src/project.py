cc1acfbc7f1f9894319fdb0d4475d7053be2526e
Thuyan Nguyen <tpn210003@utdallas.edu>
2025-12-08 09:55:02 -0600

    base function skeleton (test commit)

---
+++ src/project.py
@@ -0,0 +1,16 @@
+def main():
+    identifyFileType()
+    renameFile()
+    sortFile()
+
+def identifyFileType(type_filter):
+    {}
+
+def renameFile(filename):
+    {}
+
+def sortFile(filename):
+    {}
+
+if __name__ == '__main__':
+    main()

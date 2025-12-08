830373088e708bd6a690d10545e3ba046e69a7d8
Thuyan Nguyen <tpn210003@utdallas.edu>
2025-12-08 10:00:18 -0600

    identifyFileType bugged pass

--- src/project.py
+++ src/project.py
@@ -9,7 +9,13 @@ personal_path = r"\Personal Projects"
 unsorted_folder = r"\Users\thuya\Downloads"
 
 def identifyFileType(type_filter):
-    {}
+    {
+        type_filter = input("File Extension Requested (comma-separated, leave blank for all):").strip()
+        type_filter = {ext.lower().strip().lstrip(".")
+            for ext in type_filter.split(",")
+            if ext.strip()}
+            if type_filter else set()
+            }
 
 def renameFile(filename):
     {}
